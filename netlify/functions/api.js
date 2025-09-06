69f54a1a7ab79b6e284ae60603b2664172128624
Marija Jovic <marijajovic0709@gmail.com>
2025-09-05 18:39:05 -0700

    testing

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,18 +1,12 @@
-// Netlify Adapter File
-const serverless = require('serverless-http');
-
-// Import the Express app from our main application file
-// The '..' moves up one level out of the 'functions' folder to the project root
-const app = require('../../server.js');
-
-// Create the serverless handler with explicit configuration
-const handler = serverless(app, {
-    // This ensures the '/.netlify/functions/api' prefix is removed before passing to Express
-    request: function(request, event, context) {
-      // This is the magic line that fixes the path
-      request.path = event.path.replace('/.netlify/functions/api', '');
-      return request;
-    }
-  });
-
-  module.exports.handler = handler;
+exports.handler = async (event, context) => {
+    console.log('🔶 Function called! Path:', event.path);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ 
+        message: 'Hello from Netlify Function!',
+        receivedPath: event.path,
+        receivedQuery: event.queryStringParameters
+      }, null, 2)
+    };
+  };
